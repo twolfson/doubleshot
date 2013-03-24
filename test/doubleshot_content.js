61f2dc0381668a01fe6bbb13dff02ece916c2349
Todd Wolfson <todd@twolfson.com>
2013-03-23 18:38:53 -0700

    Getting rid of nasty hardcoded chunks

--- test/doubleshot_content.js
+++ test/doubleshot_content.js
@@ -2,11 +2,9 @@ var assert = require('assert');
 module.exports = {
   // TODO: Allow for the option to break out a topic into {before, beforeEach, after, afterEach}
   'A test': function () {
-    console.log('hey', arguments);
     this.one = 1;
   },
   'can be run': function () {
-    console.log('heybbb');
     assert(this.one, 1);
   }
 };
