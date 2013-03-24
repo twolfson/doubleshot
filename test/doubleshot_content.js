440d43003b7155c800469b8b1930ce80b7e1e5f4
Todd Wolfson <todd@twolfson.com>
2013-03-23 22:07:22 -0700

    Beginning to work with expansion tests. Failing as anticipated

--- test/doubleshot_content.js
+++ test/doubleshot_content.js
@@ -1,11 +1,17 @@
 var assert = require('assert');
 module.exports = {
   'A test': function () {
-    this.one = 1;
-    console.log('xx', this.one);
+    this.sum = 1;
   },
   'can be run': function () {
-    console.log(this.one);
-    assert.strictEqual(this.one, 1);
+    assert.strictEqual(this.sum, 1);
+  },
+  'can use aliasing': 'can be run',
+  'A test using expansion': ['Three', 'minus two'],
+  'Three': function () {
+    this.sum = 3;
+  },
+  'minus two': function () {
+    this.sum -= 2;
   }
 };
