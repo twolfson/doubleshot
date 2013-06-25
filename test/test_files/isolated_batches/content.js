966678930e52282dafa407d2876f87ddebbae889
Todd Wolfson <todd@twolfson.com>
2013-06-25 02:04:03 -0700

    Migrating tests from mocha fork in #5

---
+++ test/test_files/isolated_batches/content.js
@@ -0,0 +1,33 @@
+var assert = require('assert');
+
+module.exports = {
+  // First batch
+  'One context': function () {
+    this.context = this.context || [];
+    this.context.push(1);
+  },
+  'is isolated from other contexts': function () {
+    assert.deepEqual(this.context, [1]);
+  },
+  'nested': function () {
+    this.context.push(2);
+  },
+  'is also isolated from other contexts': function () {
+    assert.deepEqual(this.context, [1, 2]);
+  },
+
+  // Second batch
+  'Another context': function () {
+    this.context = this.context || [];
+    this.context.push(3);
+  },
+  'is isolated from the first context': function () {
+    assert.deepEqual(this.context, [3]);
+  },
+  'another nesting': function () {
+    this.context.push(4);
+  },
+  'is also isolated from the first context': function () {
+    assert.deepEqual(this.context, [3, 4]);
+  }
+};
